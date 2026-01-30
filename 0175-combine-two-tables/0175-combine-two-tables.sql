a4c75c4d92e657f01c0020167568c3fdfed0386b
Dheeraj Singh <94373786+Dheerajsingh002@users.noreply.github.com>
2026-01-30 13:02:09 +0530

    Time: 350 ms (96.3%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,2 +1,2 @@
-select p.firstname,p.lastname,a.city,a.state from Person p left join Address a on
-p.personId = a.personId;
+select p.firstName,p.LastName,a.city,a.state from Person p left join Address a
+on p.PersonId=a.PersonId
